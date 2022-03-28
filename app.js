51fbb2ddc8a27ba949d26ae15d1f52d4362be5e2
Tanishka Talwar <tanishkatalwar18@gmail.com>
2022-03-28 16:54:09 +0530

    get back on home page after composing new post and display posts on home page

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui 
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
+const posts = [];
 
 const app = express();
 app.set('view engine','ejs');
@@ -13,7 +14,7 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
 
 app.get("/",function(req,res){
-  res.render('home',{content:homeStartingContent});
+  res.render('home',{content:homeStartingContent, posts:posts});
 });
 
 app.get('/about',function(req,res){
@@ -27,9 +28,18 @@ app.get('/contact',function(req,res){
 app.get('/compose',function(req,res){
   res.render('compose');
 });
+
 app.post('/compose',function(req,res){
-  let text = req.body.composeText;
-  console.log(text);
+  const post = {
+  title : req.body.postTitle,
+  content : req.body.postText
+};
+  posts.push(post);
+  res.redirect('/');
+});
+
+app.get('/posts/:test',function(req,res){
+  console.log(req.params.test);
 })
 
 app.listen(3000,function(req,res){
