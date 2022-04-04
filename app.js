6fae48fbb536411893ea36dabd089bb6987d048f
Tanishka Talwar <tanishkatalwar18@gmail.com>
2022-04-04 12:39:48 +0530

    add local database to save posts permanently

--- app.js
+++ app.js
@@ -2,12 +2,20 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require("lodash");
+const mongoose = require("mongoose");
 
+//////////////// SETTING UP LOCAL DATABASE////////////////
+mongoose.connect("mongodb://localhost:27017/blogDB");
+const postSchema = {
+  title:String,
+  content:String
+};
+const Post = mongoose.model("Post", postSchema);
+
+/////////////////  CODE  ///////////////////
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
-
-const posts = [];
 
 const app = express();
 app.set('view engine','ejs');
@@ -15,7 +23,10 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
 
 app.get("/",function(req,res){
-  res.render('home',{content:homeStartingContent, posts:posts});
+  Post.find({},function(err,posts){
+    res.render('home',{content:homeStartingContent, posts:posts});
+  });
+
 });
 
 app.get('/about',function(req,res){
@@ -31,23 +42,23 @@ app.get('/compose',function(req,res){
 });
 
 app.post('/compose',function(req,res){
-  const post = {
-  title : req.body.postTitle,
-  content : req.body.postText
-};
-  posts.push(post);
-  res.redirect('/');
-});
-
-app.get('/posts/:test',function(req,res){
-  let requestedTitle = _.lowerCase(req.params.test);
-  posts.forEach( post => {
-    let storedTitle = _.lowerCase(post.title);
-    if( storedTitle === requestedTitle){
-      res.render('post',{post:post});
-    }
+    const post = new Post({
+    title : req.body.postTitle,
+    content : req.body.postText
+  });
+  post.save(function(err){
+      if(!err){
+          res.redirect('/');
+      }
   });
 
+});
+
+app.get('/posts/:id',function(req,res){
+    let requestedId = req.params.id;
+    Post.findById(requestedId, function(err, post){
+        res.render("post",{post:post});
+    });
 });
 
 app.listen(3000,function(req,res){
